ad645b9003fcf052143238b225de09d48ed38a4b
chandradhrmawan <chandradarmawan17@gmail.com>
2018-04-16 11:13:29 +0700

    config

---
+++ application/config/custom.php
@@ -0,0 +1,27 @@
+<?php
+
+$config['program_array'] = array(
+  'kongkow'            => 'kongkow.jpg',
+  'redbons discussion' => 'redbons-discussion.jpg',
+  'chat box'           => 'chat-box.jpg',
+  'video gokil'        => 'Video-gokil.jpg'
+);
+
+$config['program_slider'] = array(
+  'kongkow'            => 'kongkow.jpg',
+  'redbons discussion' => 'redbons-discussion.jpg',
+  'chat box'           => 'chat-box.jpg',
+  'hot seat'           => 'hot-seat.jpg'
+);
+
+$config['adv_banner']  = '<div id="div-gpt-ad-1464079991176-0" style="text-align: center; margin: 10px;">'
+                       . '<script type="text/javascript">'
+                       . 'googletag.cmd.push(function() { googletag.display("div-gpt-ad-1464079991176-0"); });'
+                       . '</script>'
+                       . '</div>';
+
+$config['adv_box']  = '<div id="div-gpt-ad-1461296073604-0" style="text-align: center; margin: 10px;">'
+                    . '<script type="text/javascript">'
+                    . 'googletag.cmd.push(function() { googletag.display("div-gpt-ad-1461296073604-0"); });'
+                    . '</script>'
+                    . '</div>';
